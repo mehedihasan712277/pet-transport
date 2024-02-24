5f7f5c67ea75e97eeff0a3d3d76579d4118c369a
mehedihasan712277 <mehedihasan712277@gmail.com>
2024-02-24 20:01:05 +0600

    cookie added

--- main.js
+++ main.js
@@ -39,12 +39,47 @@ window.addEventListener("scroll", () => {
 });
 
 // toggle text using read more button
-let toggleText = () => {
-  document.getElementById("hiddenText").classList.toggle("hiddenText");
-  let text = document.getElementById("readMoreBtn");
-  text.innerText =
-    text.innerText === "Read More..."
-      ? (text.innerText = "Read Less")
-      : "Read More...";
-};
-document.getElementById("readMoreBtn").onclick = toggleText;
+// let toggleText = () => {
+//   document.getElementById("hiddenText").classList.toggle("hiddenText");
+//   let text = document.getElementById("readMoreBtn");
+//   text.innerText =
+//     text.innerText === "Read More..."
+//       ? (text.innerText = "Read Less")
+//       : "Read More...";
+// };
+// document.getElementById("readMoreBtn").onclick = toggleText;
+
+// console.log("cookie: ", cookieValue);
+
+// function setCookie(value) {
+//   const cookieValue = document.cookie
+//     .split('=')[1]
+//   let cookieString = `product_ids=${value}|${cookieValue}`;
+
+//   document.cookie = cookieString;
+// }
+
+// function setCookie(value) {
+//   let cookieValue = document.cookie.split('=')[1] || ''; // Initialize to an empty string if cookie doesn't exist yet
+//   cookieValue = cookieValue.replace(/\|undefined/g, ''); // Remove any "|undefined" substring
+//   const cookieString = `product_ids=${value}|${cookieValue}`;
+
+//   document.cookie = cookieString;
+// }
+
+
+function setCookie(value) {
+  let cookieValue = document.cookie.split('=')[1] || ''; // Initialize to an empty string if cookie doesn't exist yet
+  cookieValue = cookieValue.replace(/\|undefined/g, ''); // Remove any "|undefined" substring
+
+  const expirationDate = new Date();
+  expirationDate.setMonth(expirationDate.getMonth() + 1); // Set expiration date to one month from now
+  const expires = expirationDate.toUTCString();
+
+  const cookieString = `product_ids=${value}|${cookieValue}; expires=${expires}; path=/`;
+
+  document.cookie = cookieString;
+}
+
+
+
